6e1d59eba305e20cf728c7d253834042c7c6c6d4
P-Andris <pillerandras0714@gmail.com>
2023-01-17 09:24:32 +0100

    AgenciesController megcsinálva

---
+++ app/Http/Controllers/AgenciesController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Agencies;
+use Illuminate\Http\Request;
+
+class AgenciesController extends Controller
+{
+    // GET:
+    public function index() {
+        $agencies = response()->json(Agencies::all());
+        return $agencies;
+    }
+
+    public function show($id) {
+        $agencies = response()->json(Agencies::find($id));
+        return $agencies;
+    }
+
+    // DELETE:
+    public function destroy($id) {
+        Agencies::find($id)->delete();
+    }
+
+    // POST:
+    public function store(Request $request) {
+        $agency = new Agencies();
+        $agency->name = $request->name;
+        $agency->country = $request->country;
+        $agency->save();
+    }
+    
+    // PUT:
+    public function update(Request $request, $id) {
+        $agency = Agencies::find($id);
+        $agency->name = $request->name;
+        $agency->country = $request->country;
+        $agency->save();
+    }
+
+    // View-ok:
+    public function newView() {
+        $agencies = Agencies::all();
+        return view('agencies.new', ['agencies' => $agencies]);
+    }
+    
+    public function editView($id) {
+        $agency = Agencies::find($id);
+        return view('agencies.edit', ['agencies' => $agency]);
+    }
+    
+    public function listView() {
+        $agencies = Agencies::all();
+        return view('agencies.list', ['agencies' => $agencies]);
+    }
+}
